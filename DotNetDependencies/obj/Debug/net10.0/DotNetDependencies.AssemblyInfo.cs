5694ed30a5b8b8a7abce2bce2e7e4f2c781b9fa1
Ariel Rojas <arielalejandrorojas@gmail.com>
2026-09-25 14:46:05 -0300

    Week 1

--- DotNetDependencies/obj/Debug/net10.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net10.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+940e6eaf5a8251ef008098597fecc53a0426933f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
